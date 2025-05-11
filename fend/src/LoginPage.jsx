6465921ce94a4130dfec71383942c70d8bbcf6b1
Katiebeo <voyentrangngn.katie@gmail.com>
2025-05-11 15:02:48 +1000

    added new page

--- fend/src/LoginPage.jsx
+++ fend/src/LoginPage.jsx
@@ -1,40 +1,42 @@
+import React from "react";
+import "./LoginPage.css";
+
 function LoginPage() {
-    return (
-      <div className="login-container">
-        <div className="login-left">
-          <h2 className="logo">Ticket<br />Please?</h2>
-          <h1>Welcome to...</h1>
-          <p>
-          Your gateway to unforgettable live experiences. 
-          From concerts and theater shows to sports events and festivals, 
-          we bring the best seats right to your screen.
-          </p>
-        </div>
-        <div className="login-right">
-          <h2>Login</h2>
-          <p>Welcome! Login to get amazing discounts and offers only for you.</p>
-          <form>
-            <label>User Name</label>
-            <input type="text" placeholder="Enter your username" />
-  
-            <label>Password</label>
-            <input type="password" placeholder="Enter your password" />
-  
-            <div className="checkbox-container">
-              <input type="checkbox" />
-              <span>Remember me</span>
-            </div>
-  
-            <button type="submit">Login</button>
-  
-            <div className="signup-link">
-              <span>New User? <a href="#">Signup</a></span>
-              <a href="#" className="forgot-password">Forgot your password?</a>
-            </div>
-          </form>
-        </div>
+  return (
+    <div className="login-container">
+      <div className="login-left">
+        <h2 className="logo">Ticket<br />Please?</h2>
+        <h1>Welcome to...</h1>
+        <p>
+          Your gateway to unforgettable live experiences. From concerts and theater shows 
+          to sports events and festivals, we bring the best seats right to your screen.
+        </p>
       </div>
-    );
-  }
-  
-  export default LoginPage;
+      <div className="login-right">
+        <h2>Login</h2>
+        <p>Welcome! Login to get amazing discounts and offers only for you.</p>
+        <form>
+          <label className="a">User Name</label>
+          <input type="text" placeholder="Enter your username" />
+
+          <label className="a">Password</label>
+          <input type="password" placeholder="Enter your password" />
+
+          <div className="checkbox-container">
+            <input type="checkbox" />
+            <span>Remember me</span>
+          </div>
+
+          <button type="submit">Login</button>
+
+          <div className="signup-link">
+            <span>New User? <a href="#">Signup</a></span>
+            <a href="#" className="forgot-password">Forgot your password?</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
