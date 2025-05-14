b13a5c306f97ecd5915b84d726c6cdb9ebb434e1
Matthew Nguyen <nducmanh08@gmail.com>
2025-05-14 19:38:09 +1000

    reorganize login page

--- fend/src/App.jsx
+++ fend/src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CreateEvents from './pages/CreateEvents/CreateEvents'; 
+import LoginPage from './pages/login/LoginPage';
 import HomePage from './pages/HomePage';
-import EventDetail from './pages/event-detail/event-detail';
-
 
 
 function App() {
@@ -11,8 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/create-event" element={<CreateEvents/>} />
-        <Route path="/event/:id" element={<EventDetail />} />
+        <Route path="/login" element={<LoginPage/>} />
       </Routes>
     </Router>
   );
