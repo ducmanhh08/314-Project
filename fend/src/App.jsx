2b4dea85fdd84032738af493a3bd057a72f75979
Katiebeo <voyentrangngn.katie@gmail.com>
2025-05-15 22:53:41 +1000

    MyTicket

--- fend/src/App.jsx
+++ fend/src/App.jsx
@@ -3,16 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateEvents from './pages/CreateEvents/CreateEvents'; 
 import HomePage from './pages/HomePage';
 import EventDetail from './pages/event-detail/event-detail';
-
+import MyTickets from './pages/MyTicket/MyTicket';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create-event" element={<CreateEvents/>} />
-        <Route path="/event/:id" element={<EventDetail />} />
+        <Route path="/my-tickets" element={<MyTickets />} />
+        {/* other routes */}
       </Routes>
     </Router>
   );
