505d45690c1739633c375f1857376cfa1b2cf8d5
Katiebeo <voyentrangngn.katie@gmail.com>
2025-05-16 00:08:49 +1000

    MyMy

--- fend/src/App.jsx
+++ fend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Attendee from './pages/Attendee';
 
 
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/att" element={<Attendee />} />
       </Routes>
     </Router>
   );
