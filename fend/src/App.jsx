1640d7efaf93975ad34333a812255bb767188d0f
Grace Chin <gcmengli@gmail.com>
2025-05-16 18:08:58 +0800

    Pushing Refund Policy page and refund images

--- fend/src/App.jsx
+++ fend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateEvents from './pages/CreateEvents/CreateEvents'; 
 import HomePage from './pages/HomePage';
 import EventDetail from './pages/event-detail/event-detail';
-
+import RefundPolicy from './pages/refund-policy/RefundPolicy';
 
 
 function App() {
@@ -13,6 +13,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/create-event" element={<CreateEvents/>} />
         <Route path="/event/:id" element={<EventDetail />} />
+        <Route path="/refund-policy" element={<RefundPolicy />} />
+        {/* Add more routes as needed */}
       </Routes>
     </Router>
   );
