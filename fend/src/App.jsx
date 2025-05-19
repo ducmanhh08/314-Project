33a369fe0f69172489f2f6b838a978396bf97f12
matildamalov <shana.yutuc@gmail.com>
2025-05-19 15:48:00 +1000

    add route to myTicket

--- fend/src/App.jsx
+++ fend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateEvents from './pages/CreateEvents/CreateEvents'; 
 import HomePage from './pages/HomePage';
+import UserHomePage from './pages/UserHomePage';
 import EventDetail from './pages/event-detail/event-detail';
 import MyTickets from './pages/MyTicket/MyTicket';
 
@@ -10,7 +11,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<UserHomePage />} />
         <Route path="/create-event" element={<CreateEvents/>} />
         <Route path="/event/:id" element={<EventDetail />} />
         <Route path="/my-tickets" element={<MyTickets />} />
