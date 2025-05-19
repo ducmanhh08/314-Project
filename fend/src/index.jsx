b1959bf35f1d8f7d6e0b69a182b7a2c16d986335
matildamalov <shana.yutuc@gmail.com>
2025-05-19 23:01:44 +1000

    all page

--- fend/src/index.jsx
+++ fend/src/index.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import './index.css';
-=======
 import { BrowserRouter } from 'react-router-dom';
->>>>>>> 84e2d5433d78c0285d08858f48aed90816fc2d91
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <App />
-<<<<<<< HEAD
-    </React.StrictMode>
-=======
     </BrowserRouter>
->>>>>>> 84e2d5433d78c0285d08858f48aed90816fc2d91
 );
