cb095078522d5813cc9fb8d69acdb43172b278d3
Katiebeo <voyentrangngn.katie@gmail.com>
2025-05-24 23:54:37 +1000

    MyMy

--- fend/src/App.jsx
+++ fend/src/App.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import LoginPage from './pages/login/LoginPage';
-import HomePage from './pages/HomePage';
-
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage/>} />
-      </Routes>
-    </Router>
-  );
-=======
 import HomePage from './page/HomePage';
-import SignUp from "./page/signUp/SignUp";
+import LoginPage from "./pages/login/LoginPage";
 
 
 
@@ -25,11 +10,10 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/login" element={<LoginPage />} />
             </Routes>
         </Router>
     );
->>>>>>> 5ee82073f07db9ea0cb8d23469e9366e5c816566
 }
 
 export default App;
--- fend/src/pages/login/LoginPage.jsx
+++ fend/src/pages/login/LoginPage.jsx
@@ -1,36 +1,37 @@
-import React from "react";
-import "./LoginPage.css";
+import { useState } from 'react'
+import './LoginPage.css'
 
 function LoginPage() {
   return (
     <div className="login-container">
       <div className="login-left">
-        <h2 className="logo">Ticket<br />Please?</h2>
+        <img src="/logo.jpg" alt="logo" className="logo-img" />
         <h1>Welcome to...</h1>
         <p>
-          Your gateway to unforgettable live experiences. From concerts and theater shows 
-          to sports events and festivals, we bring the best seats right to your screen.
+        Your gateway to unforgettable live experiences. 
+        From concerts and theater shows to sports events and festivals, 
+        we bring the best seats right to your screen.
         </p>
       </div>
       <div className="login-right">
         <h2>Login</h2>
         <p>Welcome! Login to get amazing discounts and offers only for you.</p>
         <form>
-          <label className="a">User Name</label>
-          <input type="text" placeholder="Enter your username" />
+          <label className='ha'>User Name</label>
+          <p><input type="text" placeholder="Enter your username" className='input-field'/></p>
 
-          <label className="a">Password</label>
-          <input type="password" placeholder="Enter your password" />
+          <label className='ha'>Password</label>
+          <p><input type="password" placeholder="Enter your password" className='input-field'/></p>
 
           <div className="checkbox-container">
             <input type="checkbox" />
             <span>Remember me</span>
           </div>
 
-          <button type="submit">Login</button>
+          <button type="submit" className='button'>  Login</button>
 
           <div className="signup-link">
-            <span>New User? <a href="#">Signup</a></span>
+            <span>New User? <a href="#" className='baba'>Signup</a></span>
             <a href="#" className="forgot-password">Forgot your password?</a>
           </div>
         </form>
