4a93151525c9063fc30be64145d969fe28732aaa
Katiebeo <voyentrangngn.katie@gmail.com>
2025-05-24 20:41:35 +1000

    MyMy

--- fend/src/pages/myInformation/MyInformation.jsx
+++ fend/src/pages/myInformation/MyInformation.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavbarUser from '../../components/Navbar/NavbarUser';
+import ProfileUpload from '../myInformation/ProfileUpload';
 import './MyInformation.css';
 
 function AccountInfo() {
+  const [useHomeForBilling, setUseHomeForBilling] = useState(true);
+  const [useBillingForShipping, setUseBillingForShipping] = useState(true);
+
   return (
     <div className="account-info-container">
       <NavbarUser />
@@ -11,14 +15,13 @@ function AccountInfo() {
         <h1>Account Information</h1>
       </header>
 
-      <section className="profile-photo-section">
-        <div className="profile-photo-box">
-          <div className="photo-placeholder">Profile Photo</div>
-          <button className="upload-button">Add a photo</button>
-        </div>
-      </section>
+
 
       <section className="form-section">
+        <h2>Photo Profile</h2>
+        <section className="profile-photo-section">
+          <ProfileUpload />
+        </section>
         <h2>Contact Information</h2>
         <div className="two-column">
           <input placeholder="First Name" />
@@ -32,35 +35,70 @@ function AccountInfo() {
         </div>
       </section>
 
-      {['Home', 'Billing', 'Shipping', 'Work'].map((type) => (
-        <section className="form-section" key={type}>
-          <h2>{type} Address</h2>
+          {/* Home Address */}
+       <section className="form-section">
+        <h2>Home Address</h2>
+        <div className="address-and-checkboxes">
           <div className="two-column">
             <input placeholder="Address" className='a' />
             <input placeholder="City" className='a' />
-            <input placeholder="Country"className='a'  />
+            <input placeholder="Country" className='a' />
+            <input placeholder="Postcode" className='a' />
+            <input placeholder="Territory" className='a' />
+          </div>
+
+          <div className="checkbox-group">
+            <label className="checkbox-label">
+              <input
+                type="checkbox"
+                checked={useHomeForBilling}
+                onChange={() => setUseHomeForBilling(!useHomeForBilling)}
+              />
+              Billing address is the same as home address
+            </label>
+
+            <label className="checkbox-label">
+              <input
+                type="checkbox"
+                checked={useBillingForShipping}
+                onChange={() => setUseBillingForShipping(!useBillingForShipping)}
+              />
+              Shipping address is the same as billing address
+            </label>
+          </div>
+        </div>
+      </section>
+
+
+      {/* Billing Address (conditionally shown) */}
+      {!useHomeForBilling && (
+        <section className="form-section">
+          <h2>Billing Address</h2>
+          <div className="two-column">
+            <input placeholder="Address" className='a' />
+            <input placeholder="City" className='a' />
+            <input placeholder="Country" className='a' />
             <input placeholder="Postcode" className='a' />
             <input placeholder="Territory" className='a' />
           </div>
         </section>
-      ))}
+      )}
 
-      <section className="form-section">
-        <h2>Account Email Address</h2>
-        <div className="inline-group">
-          <input placeholder='abc@gmail.com' className='b' />
-          <button className="change-button">Change</button>
-        </div>
-      </section>
 
-      <section className="form-section">
-        <h2>Your password</h2>
-        <div className="last">
-          <input placeholder="Current password" type="password" className='c'/>
-          <input placeholder="New password" type="password" className='c'/>
-          <input placeholder="Re-enter new password" type="password" className='c'/>
-        </div>
-      </section>
+      {/* Conditionally Show Shipping Address */}
+      {!useBillingForShipping && (
+        <section className="form-section">
+          <h2>Shipping Address</h2>
+          <div className="two-column">
+            <input placeholder="Address" className='a' />
+            <input placeholder="City" className='a' />
+            <input placeholder="Country" className='a' />
+            <input placeholder="Postcode" className='a' />
+            <input placeholder="Territory" className='a' />
+          </div>
+        </section>
+      )}
+
 
       <button className="save-button">Save</button>
     </div>
